cc9b7033f4c47861666f4e6bf8516a8cd221e482
Andrija Skontra <askontra22@student.unizd.hr>
2024-11-25 11:04:49 +0100

    REMOVED: route for adding user

--- src/app/api/subjects/route.ts
+++
@@ -1,9 +0,0 @@
-import { addSubjectToDB } from "@/lib/services/subject-service";
-import { NextResponse } from "next/server";
-
-export async function POST(request: Request) {
-  await addSubjectToDB(await request.json());
-  return new NextResponse("Added subject", {
-    status: 200,
-  });
-}
